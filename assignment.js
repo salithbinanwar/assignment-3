e5f685f59cfde3ce8875f04598435df04cc4b78a
pubgC71 <ernmod799@gmail.com>
2021-01-22 22:02:43 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,8 @@
+
+// kilometerToMeter...
+function kilometerToMeter(kilometer){
+
+    const meter = (kilometer * 1000);
+    return meter;
+} 
+//console.log(kilometerToMeter(7));      Kilometer is "7000"
